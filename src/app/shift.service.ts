b253df1d8331a4338aea402215347cb2700aac0a
aeginidae <aeginidae@gmail.com>
2019-03-15 12:01:39 -0500

    shift service makes request

--- src/app/shift.service.ts
+++ src/app/shift.service.ts
@@ -1,15 +1,34 @@
 import { Injectable } from '@angular/core';
 import data from 'mockDataShift.json';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'content-type': 'application/json' }),
+};
+const serverUrl = 'http://localhost:4001';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShiftService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
   /**
    * @todo make network request for shifts from DB
    * @todo change type signature to Array<Shift>
    */
   allShifts: Array<any> = data;
+  private extractData(res: Response) {
+    return res || {};
+  }
+  getAllShifts(): Observable<any> {
+    return this.http.get(`${serverUrl}/shifts`, httpOptions).pipe(
+      map(this.extractData),
+      catchError(err => throwError(err))
+    );
+  }
 }
