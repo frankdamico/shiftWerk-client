6664c09f29234c9947a817c9e8479cd261016b0d
aeginidae <aeginidae@gmail.com>
2019-03-14 20:05:43 -0500

    navbar works!

--- src/app/user/user-navbar/user-navbar.component.ts
+++ src/app/user/user-navbar/user-navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-user-navbar',
@@ -8,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class UserNavbarComponent implements OnInit {
 
   constructor() { }
-
+  @Output() NavClick = new EventEmitter<string>();
+  onClick(view: string) {
+    this.NavClick.emit(view);
+  }
   ngOnInit() {}
 
 }
