14462cec58e7c5db9860247a659f2db4d24b90dc
aeginidae <aeginidae@gmail.com>
2019-03-14 20:18:15 -0500

    document maker service

--- src/app/maker.service.ts
+++ src/app/maker.service.ts
@@ -7,7 +7,19 @@ import data from 'mockDataMaker.json';
 export class MakerService {
 
   constructor() { }
+  /**
+   * @todo make network request for makers from DB
+   * @todo change type signature to Array<Maker>
+   */
   allMakers: Array<any> = data;
+
+  /** @method getMakerById
+   * gets maker from present data by id
+   * @todo make network request
+   * @todo return Promise<Maker>
+   *
+   * @param id - db-generated id
+   */
   getMakerById(id: Number) {
     return this.allMakers.find(maker => maker.id === id);
   }
