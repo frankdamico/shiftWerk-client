26054eb6672f4d6c5ebfccda159b4426a73f92ca
Taniecia Duplantis <duplantist@gmail.com>
2019-03-16 16:18:21 -0500

    search works

--- src/app/maker/maker-search/maker-search.component.ts
+++ src/app/maker/maker-search/maker-search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-maker-search',
@@ -7,8 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MakerSearchComponent implements OnInit {
 
+  public searchTerm: string
   constructor() { }
-
+  
   ngOnInit() {}
 
+  /**
+   * @method searchList
+   * passes the search terms to the server service
+   */
+  searchList(event) {
+    // this.searchTerm = value;
+    console.log(event.detail.value);
+  }
 }
