de43f3da111ee7e464dda9ef2c51cf8765f9d88c
Frank D'Amico <frankadamicoaf@gmail.com>
2019-03-15 19:27:01 -0500

    maker-home.component .html & .ts - add ion & angular components

--- src/app/maker/maker-home/maker-home.component.ts
+++ src/app/maker/maker-home/maker-home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-maker-home',
@@ -9,6 +9,7 @@ export class MakerHomeComponent implements OnInit {
 
   constructor() { }
 
+  @Input() shifts: Array<any>;
   ngOnInit() {}
 
 }
