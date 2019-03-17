90cc8407fa26d35b0262d24a47cb3eaf4aaa976a
David <dlum988@gmail.com>
2019-03-16 22:37:55 -0500

    added toast to save

--- src/app/werker/werker-profile/werker-profile.component.ts
+++ src/app/werker/werker-profile/werker-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-werker-profile',
@@ -13,20 +14,34 @@ export class WerkerProfileComponent implements OnInit {
   public nameLast:string;
   public email:string;
   public phoneNumber:number;
-  public positions:string[] = []
+  public positions:string[] = [];
   public availability:boolean = false;
+  public bio:string;
 
-  constructor() { }
+  constructor(
+    public toastController: ToastController
+  ) { }
   
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'Your settings have been saved.',
+      duration: 2000,
+      color: 'primary',
+      position: 'top'
+    });
+    toast.present();
+  }
+
   lastMinAvail() {
     this.availability = !this.availability;
   }
   werkerPosition(position) {
     const index = this.positions.indexOf(position);
-    index >= 0 ? this.positions.splice(index, 1) : this.positions.push(position);
+    index === -1 ? this.positions.push(position) : this.positions.splice(index, 1);
   }
-  onSave() {
+  saveSettings() {
     console.log("I'm saving");
+    this.presentToast();
   }
   ngOnInit() {}
 
