b260404c880f6ae04d318079026925319662e03a
David <dlum988@gmail.com>
2019-03-18 11:30:24 -0500

    added toast

--- src/app/werker/werker-shift-expanded/werker-shift-expanded.component.ts
+++ src/app/werker/werker-shift-expanded/werker-shift-expanded.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-werker-shift-expanded',
@@ -7,11 +8,22 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class WerkerShiftExpandedComponent implements OnInit {
 
-  constructor() { }
+  constructor(public toastController: ToastController) { }
   @Input() shift: object;
+
+  async presentToast(answer) {
+    const toast = await this.toastController.create({
+      message: `Invitation ${answer}`,
+      duration: 2000,
+      color: 'primary',
+      position: 'top'
+    });
+    toast.present();
+  }
 
   acceptOrDecline(answer) {
     console.log(answer);
+    this.presentToast(answer);
   }
   ngOnInit() {
   }
