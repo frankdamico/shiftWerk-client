b5d5ac666b29700c451385cfb1925902cc6e7032
David <dlum988@gmail.com>
2019-03-19 20:10:36 -0500

    created past shift component

---
+++ src/app/werker/werker-past-shift/werker-past-shift.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-werker-past-shift',
+  templateUrl: './werker-past-shift.component.html',
+  styleUrls: ['./werker-past-shift.component.scss'],
+})
+export class WerkerPastShiftComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+  @Input()
+  // pastShifts: Array<any>;
+  // upcomingShifts: Array<any>;
+  // invitedShifts: Array<any>;
+}
