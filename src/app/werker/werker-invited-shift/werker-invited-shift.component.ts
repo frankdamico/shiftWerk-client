a60129d84a43bcdb1dd73f59dbd10b10ffd57be1
David <dlum988@gmail.com>
2019-03-19 20:41:00 -0500

    passing down invited shifts to each individual shift

--- src/app/werker/werker-invited-shift/werker-invited-shift.component.ts
+++ src/app/werker/werker-invited-shift/werker-invited-shift.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-werker-invited-shift',
@@ -8,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class WerkerInvitedShiftComponent implements OnInit {
 
   constructor() { }
+
+  @Input()
+  invitedShift: Array<any>;
 
   ngOnInit() {}
 
