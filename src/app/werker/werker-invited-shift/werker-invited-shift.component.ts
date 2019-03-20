30ec1419271a1cbb116e74c674592ab1198741be
David <dlum988@gmail.com>
2019-03-19 20:26:56 -0500

    created werker shift component

---
+++ src/app/werker/werker-invited-shift/werker-invited-shift.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-werker-invited-shift',
+  templateUrl: './werker-invited-shift.component.html',
+  styleUrls: ['./werker-invited-shift.component.scss'],
+})
+export class WerkerInvitedShiftComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
