58500566a538b5d49a13a77342c61ff411d9d27a
David <dlum988@gmail.com>
2019-03-20 21:24:37 -0500

    receiving pastShifts into component

--- src/app/werker/werker-history/werker-history.component.ts
+++ src/app/werker/werker-history/werker-history.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-werker-history',
@@ -7,8 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WerkerHistoryComponent implements OnInit {
 
+  @Input()
+  pastShifts: Array<Object>;
+
   constructor() { }
 
   ngOnInit() {}
 
+  rateShift(shift) {
+    console.log(shift);
+  }
 }
