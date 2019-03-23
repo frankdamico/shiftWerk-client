123a23d36208ff349b6d56a5412e1cc10744962b
aeginidae <aeginidae@gmail.com>
2019-03-23 11:07:55 -0500

    documentation

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -26,9 +26,16 @@ export class AuthService {
   public user: Werker | Maker;
   _webClientId: String = '347712232584-9dv95ud3ilg9bk7vg8i0biqav62fh1q7.apps.googleusercontent.com';
 
+  /**
+   * handles pipeline of login functions
+   * executes each function in sequence
+   *
+   * @param role - either 'werker' or 'maker'
+   */
   public login(role: string): Observable<any> {
     return this.signIn(role)
       .pipe(
+        // res.signIn is method from GoogleAuthService
         concatMap(res => res.signIn()),
         concatMap(res => this.signInSuccess(res, role)),
         concatMap(() => this.getToken()),
@@ -36,12 +43,23 @@ export class AuthService {
       );
   }
 
+  /**
+   * uses GoogleAuthService to request permissions from user
+   *
+   * @param role - either 'werker' or 'maker'
+   */
   private signIn(role: string): Observable<any> {
     return this.googleAuth.getAuth()
       .pipe(
         catchError(err => throwError(err)));
   }
 
+  /**
+   * stores relevant info in local storage
+   *
+   * @param res - google auth response
+   * @param role - either 'werker' or 'maker'
+   */
   private signInSuccess(res, role: string): Observable<any> {
     alert(JSON.stringify(res));
     const token = res.getAuthResponse();
@@ -80,6 +98,12 @@ export class AuthService {
     ).pipe(catchError(err => throwError(err)));
   }
 
+  /**
+   * sends access token to API server for verification and storage
+   *
+   * @param token - access_token from google
+   * @param role - either 'werker' or 'maker'
+   */
   private saveLogin(token: string, role: string): Observable<any> {
     console.log(token);
     const endpoint = role === 'werker' ? 'werkers' : 'makers';
@@ -87,32 +111,63 @@ export class AuthService {
       .pipe(catchError(err => throwError(err)));
   }
 
+  /**
+   * gets user from local storage
+   */
   private getLocalUserInfo(): Observable<Werker | Maker> {
     return from(this.storage.get('USER'))
       .pipe(catchError(err => throwError(err)));
   }
 
+  /**
+   * gets access token from local storage
+   */
   private getToken(): Observable<string> {
     return from(this.storage.get(AuthService.STORAGE_KEY));
   }
 
-  private verifyUser(token: string): Observable<any> {
+  /**
+   * submits access token to API server for verification
+   * res should have either user info or notification
+   * that user is not verified
+   *
+   * @todo make this work
+   *
+   * @param token - access token from google
+   * @param role - either 'werker' or 'maker'
+   */
+  private verifyUser(token: string, role: string): Observable<any> {
     return this.http.put('/login', token, httpOptions);
   }
 
+  /**
+   * updates local storage with new data
+   *
+   * @param values - object with new values for local user
+   */
   private updateLocalUserInfo(values: Object): Observable<any> {
     return from(this.storage.set(
       AuthService.USER, Object.assign(this.user, values)
       ));
   }
 
+  /**
+   * updates API server with new data on user
+   *
+   * @param values - object with new values for server
+   */
   private updateServerUserInfo(user: Werker | Maker, values: Object): Observable<any> {
     return this.http.patch(`${serverUrl}/settings`, Object.assign(user, values), httpOptions)
       .pipe(catchError(err => throwError(err)));
   }
 
+  /**
+   * gets a default user for demonstration purposes
+   *
+   * @param role - either 'werker' or 'maker'
+   */
   public getDefaultUser(role: string): Observable<any> {
-    return this.http.get(`${serverUrl}/${role}/1`)
+    return this.http.get(`${serverUrl}/${role}/5`)
       .pipe(catchError(err => throwError(err)));
   }
 }
