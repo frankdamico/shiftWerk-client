19bb97fdb46950292d772b76bd63225681b152ea
aeginidae <aeginidae@gmail.com>
2019-03-23 14:08:06 -0500

    include guard for werker-home and maker-home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,8 +7,8 @@ const routes: Routes = [
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   // { path: 'new-user', loadChildren: './new-user/new-user.module#NewUserPageModule' },
   { path: 'page-not-found', loadChildren: './page-not-found/page-not-found.module#PageNotFoundPageModule' },
-  { path: 'werker-home', loadChildren: './werker/werker.module#WerkerPageModule' },
-  { path: 'maker-home', loadChildren: './maker/maker.module#MakerPageModule' },
+  { path: 'werker-home', loadChildren: './werker/werker.module#WerkerPageModule', canActivate: [AuthGuard] },
+  { path: 'maker-home', loadChildren: './maker/maker.module#MakerPageModule', canActivate: [AuthGuard] },
   { path: '**', redirectTo: 'page-not-found' },
 ];
 
