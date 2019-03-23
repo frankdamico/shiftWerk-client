aa56960cbbcb99129152b8ad4ec52ddb0e3430b8
aeginidae <aeginidae@gmail.com>
2019-03-23 14:47:46 -0500

    notes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ const routes: Routes = [
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   // { path: 'new-user', loadChildren: './new-user/new-user.module#NewUserPageModule' },
   { path: 'page-not-found', loadChildren: './page-not-found/page-not-found.module#PageNotFoundPageModule' },
+  // delete canActivates to disable authentication
   { path: 'werker-home', loadChildren: './werker/werker.module#WerkerPageModule', canActivate: [AuthGuard] },
   { path: 'maker-home', loadChildren: './maker/maker.module#MakerPageModule', canActivate: [AuthGuard] },
   { path: '**', redirectTo: 'page-not-found' },
