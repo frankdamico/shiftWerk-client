3c90d01a7be825d19689c0d363c39b18b8eba736
aeginidae <aeginidae@gmail.com>
2019-03-25 11:50:03 -0500

    maker page now correctly assigns its maker

--- src/app/maker/maker.page.ts
+++ src/app/maker/maker.page.ts
@@ -45,7 +45,7 @@ export class MakerPage implements OnInit {
     const loading = await this.loadingController.create();
     await loading.present();
     // makers id is 1
-    await this.authService.getDefaultUser('makers', 1)
+    await this.authService.getDefaultUser('makers')
       .subscribe(res => {
         console.log('MEOW');
         console.log(res);
@@ -119,6 +119,7 @@ export class MakerPage implements OnInit {
         loading.present();
         this.authService.getDefaultUser('makers')
           .pipe(
+            tap(maker => this.maker = maker),
             concatMap(maker => forkJoin(
               this.makerService.getApplications(maker.id),
               this.makerService.getHistory(maker.id),
