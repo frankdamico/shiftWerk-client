520e80ad57f5d624c144baa9817422e441ac1c34
Taniecia Duplantis <duplantist@gmail.com>
2019-03-25 16:13:02 -0500

    changed button color

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -2,16 +2,13 @@ import { Component, OnInit, Input } from '@angular/core';
 import { MakerService } from '../maker.service'
 
 @Component({
-  selector: 'app-favorite',
-  templateUrl: './favorite.component.html',
-  styleUrls: ['./favorite.component.scss'],
+  selector: "app-favorite",
+  templateUrl: "./favorite.component.html",
+  styleUrls: ["./favorite.component.scss"]
 })
 export class FavoriteComponent implements OnInit {
-
-  buttonColor: string = '#000';
-  constructor(
-    private makerService: MakerService
-  ) { }
+  buttonColor: string = "#4e4747";
+  constructor(private makerService: MakerService) {}
 
   ngOnInit() {}
 
@@ -19,20 +16,24 @@ export class FavoriteComponent implements OnInit {
   @Input() werkers: any;
   @Input() i: any;
 
-  favorite = (i) => {
-    let ids = { makerId: this.maker['id'], werkerId: this.werkers[i].id, type: 'maker' };
-    if (this.buttonColor === '#000') {
-      this.buttonColor = '#dddddd'
-      console.log('fav');
+  favorite = i => {
+    let ids = {
+      makerId: this.maker["id"],
+      werkerId: this.werkers[i].id,
+      type: "maker"
+    };
+    if (this.buttonColor === "#4e4747") {
+      this.buttonColor = "#ff6666";
+      console.log("fav");
       this.makerService.fav(ids).subscribe(response => {
         console.log(response);
-      })
+      });
     } else {
-      this.buttonColor = '#000'
-      console.log('unfav');
+      this.buttonColor = "#4e4747";
+      console.log("unfav");
       this.makerService.unFav(ids).subscribe(response => {
         console.log(response);
-      })
+      });
     }
-  }
+  };
 }
