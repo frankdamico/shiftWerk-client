525a929d557f67004c181f4aefa6dd9afb2d3a62
Taniecia Duplantis <duplantist@gmail.com>
2019-03-26 20:04:48 -0500

    passed down maker to get the maker id

--- src/app/maker/maker-unfilled-shifts/maker-unfilled-shifts.component.ts
+++ src/app/maker/maker-unfilled-shifts/maker-unfilled-shifts.component.ts
@@ -21,6 +21,7 @@ export class MakerUnfilledShiftsComponent implements OnInit {
   ) { }
 
   @Input() shifts: Array<any>;
+  @Input() maker: any;
   // unfilled: Array<any>;
   // unfulfulled: Array<any>;
 
@@ -44,6 +45,7 @@ export class MakerUnfilledShiftsComponent implements OnInit {
   }
 
   ngOnInit() {
+    console.log(this.maker)
     // this.shiftService.getUpcomingShifts();
     // this.makerService.getUnfulfilledShifts();
   }
