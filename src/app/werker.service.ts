d340762403942f51290e0d30c3640d89c6a6c337
David <dlum988@gmail.com>
2019-03-28 11:39:38 -0500

    using hosted serverURL

--- src/app/werker.service.ts
+++ src/app/werker.service.ts
@@ -9,7 +9,7 @@ const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 };
 const serverUrl = 'http://35.185.77.220:4000';
-// const serverUrl = 'http://localhost:4001';
+// const serverUrl = 'http://localhost:4000';
 @Injectable({
   providedIn: 'root'
 })
