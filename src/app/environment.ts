5b53237c4f3721a4366554592a5602d7854161f9
aeginidae <aeginidae@gmail.com>
2019-03-28 21:19:23 -0500

    move back to deployment server

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,7 +1,7 @@
 import { HttpHeaders } from '@angular/common/http';
 
-// const serverUrl = 'http://35.185.77.220:4000';
-const serverUrl = 'http://localhost:4000';
+const serverUrl = 'http://35.185.77.220:4000';
+// const serverUrl = 'http://localhost:4000';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
